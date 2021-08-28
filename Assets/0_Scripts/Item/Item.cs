0289bcf6e7443d4a987ad1d61d3c4074300b50e1
PakaDopa <detols33@naver.com>
2021-08-28 17:47:32 +0900

    아이템 스포너 기능

--- Assets/0_Scripts/Item/Item.cs
+++ Assets/0_Scripts/Item/Item.cs
@@ -7,17 +7,6 @@ public abstract class Item : MonoBehaviour
     [SerializeField] protected Collider2D _collider;
     [SerializeField] protected ItemView _view;
 
-    [SerializeField] private Sprite itemSprite;
-
-    public void Awake()
-    {
-        if (itemSprite == null)
-            return;
-
-        var sprite = gameObject.GetComponent<Image>()?.sprite;
-        if (sprite == null)
-            sprite = itemSprite;
-    }
     public abstract void ApplyEffect(Actor actor);
 
     public void DestroySelf()
--- Assets/0_Scripts/ItemRespawn.cs
+++ Assets/0_Scripts/ItemRespawn.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class ItemRespawn : MonoBehaviour
 {
-    public float respawnTime = 4.0f;
+    public float respawnTime = 10.0f;
+    public GameObject healthItem;
+    public GameObject levelUpItem;
 
     private void Start()
     {
@@ -12,12 +14,34 @@ public class ItemRespawn : MonoBehaviour
     }
     IEnumerator RespawnItemCo()
     {
+        
         var tileMap = FindObjectOfType(typeof(TileMap)) as TileMap;
         while (true)
         {
             float delay = Random.Range(1, respawnTime);
             yield return new WaitForSeconds(delay);
-            Debug.Log("아이템 생성!!  " + delay);
+            //debugging
+            int randX = Random.Range(0, 8);
+            int randY = Random.Range(0, 5);
+            int type = Random.Range(0, 2);
+            var itemPrefab = default(GameObject);
+            if (type == 0)
+                itemPrefab = healthItem;
+            else
+                itemPrefab = levelUpItem;
+            Debug.Log("아이템 생성!!  이전 딜레이: [" + randX + ", " + randY + "]");
+
+            //get tile position
+            Vector3 tile = tileMap[randX, randY].transform.position;
+
+            //복제
+            if (itemPrefab is null)
+                continue;
+            var obj = Instantiate(itemPrefab, new Vector3(tile.x, tile.y,
+                0), Quaternion.identity);
+            obj.transform.localScale = new Vector2(0.3f, 0.3f);
+            Debug.Log(obj.name);
+            
         }
     }
 }
