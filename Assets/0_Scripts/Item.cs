4e898f9b5b5649778e99495cd9b9450d43eca780
yellowisher <auau52@naver.com>
2021-08-28 12:56:24 +0900

    아이템 기본 기능 개발

---
+++ Assets/0_Scripts/Item.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public abstract class Item : MonoBehaviour
+{
+    [SerializeField] protected Collider2D _collider;
+    [SerializeField] protected ItemView _view;
+
+    public abstract void ApplyEffect(Actor actor);
+
+    public void DestroySelf()
+    {
+        _collider.enabled = false;
+        Destroy(gameObject);
+    }
+}
