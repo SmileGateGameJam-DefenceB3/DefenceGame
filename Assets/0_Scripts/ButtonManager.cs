37c2b35e34e4fcf1234f90b5223aade635d04147
PakaDopa <detols33@naver.com>
2021-08-29 06:10:39 +0900

    메인 화면

--- Assets/0_Scripts/ButtonManager.cs
+++ Assets/0_Scripts/ButtonManager.cs
@@ -5,21 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
 {
-    public void GameStart(string sceneName)
-    {
-        Debug.Log("!!");
-        SceneManager.LoadScene(sceneName);       
-    }
-    public void GameExit()
+    public Animator transition;
+    public float transitionTime = 1.0f;
+    public void Exit()
     {
 
+        StartCoroutine(GameExitCo());
     }
-    public void ShowCredit()
+    public void ChangeScene(string sceneName)
     {
+        StartCoroutine(LoadScene(sceneName));
+    }
 
-    }
-    public void GameSetting()
+    IEnumerator LoadScene(string sceneName)
     {
+        //play animation
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(sceneName);
+    }
+    IEnumerator GameExitCo()
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
 
     }
 }
